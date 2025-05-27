0cfc9a940c7e837b5083cb5ae4636a31da258edb
ArthurSonin <sonin.artur@chnu.edu.ua>
2025-05-27 08:21:56 +0300

    Зроблено 1, 2 і 3 завдання

--- ArrayFile/ArrayFile.cpp
+++ ArrayFile/ArrayFile.cpp
@@ -1,4 +1,7 @@
-﻿ // ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+// ArrayFile.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -16,11 +19,11 @@ const int MAX_SIZE = 560;
 
 /*
 *   ConsoleInputArrayDouble
-*   
+*
 */
 int ConsoleInputSizeArray(const int sizeMax)
 {
-    int size = 0; 
+    int size = 0;
     do {
         cout << " Input size Array ( 0< 1 < " << sizeMax << " ) ";
         cin >> size;
@@ -34,7 +37,7 @@ int ConsoleInputSizeArray(const int sizeMax)
 int ConsoleInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-        for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> A[i];
     }
     return size;
@@ -47,27 +50,39 @@ int ConsoleInputArray(int sizeMax, double A[])
 int RndInputArray(int sizeMax, double A[])
 {
     int size = ConsoleInputSizeArray(sizeMax);
-    int r1=0, r2=0;
+    int r1 = 0, r2 = 0;
     srand(size);
 
     for (int i = 0; i < size; i++) {
+<<<<<<< Updated upstream
         if (i < size/2) A[i] = rand() % 100;
+=======
+        if (i < size / 2) A[i] = rand() % 100;
+>>>>>>> Stashed changes
         else {
             r1 = rand() % 100;
             r2 = rand() % 100;
             A[i] = r1 - r2;
 
         }
+<<<<<<< Updated upstream
       /* r1 = rand();
         r2 = rand();
         A[i] = 100.0 * r1;
         A[i] = A[i] / (1.0 + r2);
         cout << A[i] << "   ";*/
+=======
+        /* r1 = rand();
+          r2 = rand();
+          A[i] = 100.0 * r1;
+          A[i] = A[i] / (1.0 + r2);
+          cout << A[i] << "   ";*/
+>>>>>>> Stashed changes
     }
     return size;
 }
 
-int ConsoleInputDynamicArrayNew(int sizeMax, pDouble &pA)
+int ConsoleInputDynamicArrayNew(int sizeMax, pDouble& pA)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     pA = new double[size];
@@ -89,23 +104,23 @@ int ConsoleInputDynamicArray_calloc(int sizeMax, pDouble& pA)
     return size;
 }
 
-void ConsoleInputVector(int sizeMax, vector<double> &A)
+void ConsoleInputVector(int sizeMax, vector<double>& A)
 {
     int size = ConsoleInputSizeArray(sizeMax);
     double d;
     for (int i = 0; i < size; i++) {
         cout << " Array[ " << i << "] "; cin >> d; A.push_back(d);
     }
-    return ;
+    return;
 }
 
 
 /*
-*  WriteArrayTextFile 
+*  WriteArrayTextFile
 *
 */
 
-void WriteArrayTextFile(int n, double *arr, const char *fileName )
+void WriteArrayTextFile(int n, double* arr, const char* fileName)
 {
     ofstream fout(fileName);
     if (fout.fail()) return;
@@ -127,12 +142,16 @@ int ReadArrayTextFile(int n, double* arr, const char* fileName)
     if (fin.fail()) return 0;
     fin >> size;
     if (size <= 0) return 0;
-    if (size > n) size = n;   
+    if (size > n) size = n;
     for (int i = 0; i < n; i++)
+<<<<<<< Updated upstream
        fin >> arr[i];
+=======
+        fin >> arr[i];
+>>>>>>> Stashed changes
     fin.close();
     return size;
-    
+
 }
 
 
@@ -143,14 +162,14 @@ void WriteArrayBinFile(int n, double* arr, const char* fileName)
     ofstream bfout(fileName, ios_base::binary);
     if (bfout.fail()) return;
     bfout.write((const char*)&n, sizeof(int));
-    std::streamsize  cn = static_cast<std::streamsize>(n) *sizeof(double);
+    std::streamsize  cn = static_cast<std::streamsize>(n) * sizeof(double);
     bfout.write((const char*)arr, cn);
     bfout.close();
 }
 
 int ReadArrayBinFile(int n, double* arr, const char* fileName)
 {
-    int size=0;
+    int size = 0;
     ifstream bfin(fileName, ios_base::binary);
     if (bfin.fail()) return 0;
     bfin.read((char*)&size, sizeof(int));
@@ -171,7 +190,11 @@ void ShowMainMenu()
     cout << "    4.  cls (очищення екрану) \n";
     cout << "    5.  Зчитування в контейнер \n";
     cout << "    0.  Вихiд \n";
+<<<<<<< Updated upstream
   }
+=======
+}
+>>>>>>> Stashed changes
 
 void MenuTask()
 {
@@ -195,13 +218,13 @@ void MenuInput()
 
 
 /*
-* Задано одновимірний масив А розміру 2N. 
-* Побудувати два масиви В і С розміру N, 
+* Задано одновимірний масив А розміру 2N.
+* Побудувати два масиви В і С розміру N,
 * включивши  у масив В елементи масиву А з парними індексами,
 * а у С - з непарними.
 *****************
-*  A - in 
-*  B, C - out 
+*  A - in
+*  B, C - out
 */
 void  TestVariant(int N, double* A, double* B, double* C) {
     for (int i = 0; i < N; i++) {
@@ -211,8 +234,8 @@ void  TestVariant(int N, double* A, double* B, double* C) {
 }
 /*
 *  Task  Var
-* 
-* 
+*
+*
 */
 void TaskV()
 {
@@ -222,16 +245,16 @@ void TaskV()
         MenuTask();
         ch = getchar();
         getchar();
-            switch (ch) {
-             case '1': cout << " 1 "; break;
-             case '2': cout << " 2 "; break;
+        switch (ch) {
+        case '1': cout << " 1 "; break;
+        case '2': cout << " 2 "; break;
             //
-            case '5': return;
-            }
+        case '5': return;
+        }
         cout << " Press any key and enter\n";
         ch = getchar();
-        } while (ch != 27);
-    
+    } while (ch != 27);
+
 }
 
 void ArrayLocal()
@@ -257,6 +280,7 @@ void ArrayLocal()
 }
 
 void ArthurTask1() {
+<<<<<<< Updated upstream
     pDouble B; 
     int m;
     double sm=0, sp=0, smp=0;
@@ -429,22 +453,201 @@ int main(){
     n = RndInputArray(MAX_SIZE, A);
     WriteArrayTextFile(n, A, "1.txt");
     m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
+=======
+    pDouble B;
+    int m;
+    double sm = 0, sp = 0, smp = 0;
+    B = new double[MAX_SIZE];
+    if (B == nullptr) { return; }
+    m = ReadArrayTextFile(MAX_SIZE, B, "task.txt");
+>>>>>>> Stashed changes
     cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++) {
         cout << B[i] << "   ";
-    WriteArrayBinFile(n, A, "1.bin");
-    m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
-    cout << " \n m= " << m << endl;
-    for (int i = 0; i < m; i++)
-        cout << C[i] << "   ";
-    cout << "\n  Vector \n";
-    vector<double> vA;
-    ConsoleInputVector(MAX_SIZE, vA);
-    for (auto v : vA) {
-        cout << v << "   ";
+        if (B[i] < 0) sm += B[i];
+        else sp += B[i];
+        smp = sp - sm;
     }
+<<<<<<< Updated upstream
     */
    // TaskV();
+=======
+    cout << "\n рiзниця = " << smp << "\n\n";
+
+    ofstream fout("task1out.txt");
+    if (fout.fail()) return;
+    fout << smp << endl;
+    fout.close();
+
+
+}
+
+void ArthurTask2() {
+    pDouble B;
+    int m, i;
+    int k1 = 0, max = 0;
+    cout << "\n Введiть число кратностi: ";
+    cin >> k1;
+    B = new double[MAX_SIZE];
+    if (B == nullptr) { return; }
+    m = ReadArrayTextFile(MAX_SIZE, B, "task.txt");
+    cout << " \n m= " << m << endl;
+    for (i = 0; i < m; i++) {
+        cout << B[i] << "   ";
+    }
+    i = 0;
+    do {
+        if ((int)B[i] % (int)k1 == 0) {
+            if (max < B[i]) max = B[i];
+        }
+        i++;
+    } while (B[i] >= 0 && i < m);
+    cout << "\nmax= " << max << endl;
+
+    ofstream fout("task2out.txt");
+    if (fout.fail()) return;
+    fout << max << endl;
+    fout.close();
+
+
+}
+
+void ArthurTask3() {
+    pDouble B;
+    int C[400];
+    int m, n, i, j, l = 0, y;
+    B = new double[MAX_SIZE];
+    if (B == nullptr) { return; }
+    m = ReadArrayTextFile(MAX_SIZE, B, "task.txt");
+    cout << " \n m= " << m << endl;
+    for (i = 0; i < m; i++) {
+        cout << B[i] << "   ";
+    }
+    cout << "\n\n";
+    for (i = 0; i < m; i++) {
+        for (j = i + 1; j < m; j++) {
+            if (B[i] == B[j]) {
+                y = 0;
+                for (n = 0; n < l; n++) {
+                    if (C[n] == B[j]) y = 1;
+
+                }
+                if (y == 0) {
+                    C[l] = B[i];
+                    l += 1; break;
+                }
+            }
+            else;
+        }
+    }
+    for (i = 0; i < l; i++) {
+        cout << C[i] << "   ";
+        if ((i + 1) % 7 == 0) {
+            cout << endl;
+        }
+    }
+    cout << endl << endl;
+    ofstream fout("task3out.txt");
+    if (fout.fail()) return;
+    for (i = 0; i < l; i++) {
+        fout << C[i] << "   ";
+        if ((i + 1) % 7 == 0) {
+            fout << endl;
+        }
+    }
+    fout.close();
+
+}
+
+
+
+
+int main() {
+    int size;
+    double d;
+    vector <double> A;
+    ifstream fin;
+    int m, t, s;
+    double B[MAX_SIZE];
+    setlocale(LC_ALL, "RU");
+
+    do {
+        ShowMainMenu();
+        cin >> m;
+        if (m != 4 && m != 0 && m != 5) {
+            MenuTask();
+            cin >> t;
+            if (t == 1) {
+                s = ConsoleInputArray(MAX_SIZE, B);
+                WriteArrayTextFile(s, B, "task.txt");
+            }
+            if (t == 2) {
+                s = RndInputArray(MAX_SIZE, B);
+                // WriteArrayBinFile(s, B, "task.txt");
+                WriteArrayTextFile(s, B, "task.txt");
+            }
+        }
+        switch (m)
+        {
+        case 1:
+            ArthurTask1();
+            break;
+        case 2:
+            ArthurTask2();
+            break;
+        case 3:
+            ArthurTask3();
+            break;
+        case 4:
+            system("cls");
+            break;
+        case 5:
+
+            fin.open("task.txt");
+            if (fin.fail()) break;
+            fin >> size;
+            if (size <= 0) break;
+            for (int i = 0; i < size; i++) {
+                fin >> d;
+                A.push_back(d);
+            }
+            fin.close();
+
+            for (int i = 0; i < A.size(); i++) {
+                cout << " " << A[i] << " ";
+            }
+            cout << "\n\n";
+            break;
+        default:
+            break;
+
+        }
+    } while (m != 0);
+
+
+    /*
+        double A[MAX_SIZE], B[MAX_SIZE],C[MAX_SIZE];
+        int n,m;
+        n = RndInputArray(MAX_SIZE, A);
+        WriteArrayTextFile(n, A, "1.txt");
+        m = ReadArrayTextFile(MAX_SIZE, B, "1.txt");
+        cout << " \n m= " << m << endl;
+        for (int i = 0; i < m; i++)
+            cout << B[i] << "   ";
+        WriteArrayBinFile(n, A, "1.bin");
+        m = ReadArrayBinFile(MAX_SIZE, C, "1.bin");
+        cout << " \n m= " << m << endl;
+        for (int i = 0; i < m; i++)
+            cout << C[i] << "   ";
+        cout << "\n  Vector \n";
+        vector<double> vA;
+        ConsoleInputVector(MAX_SIZE, vA);
+        for (auto v : vA) {
+            cout << v << "   ";
+        }
+        */
+        // TaskV();
+>>>>>>> Stashed changes
     return 1;
 
 }
